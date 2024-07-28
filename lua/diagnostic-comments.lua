152d74d9d03d7cd78a6f047718667ac87225388c
Ken Andrew Taylor <kentaylorappdev@gmail.com>
2024-07-28 09:52:29 -0400

    added comment prefix

--- lua/diagnostic-comments.lua
+++ lua/diagnostic-comments.lua
@@ -1,20 +1,19 @@
 local M = {}
 
--- Move namespace creation to the top
 M.namespace = vim.api.nvim_create_namespace("diagnostic_comments")
 
 M.config = {
 	comment_style = "above", -- 'above' or 'inline'
 	keymap = "<leader>dc", -- default keymap to toggle diagnostic comments
+	comment_prefix = "--", -- new option for comment prefix
 }
 
 function M.setup(opts)
 	M.config = vim.tbl_deep_extend("force", M.config, opts or {})
-	-- Set up the keymap
 	vim.api.nvim_set_keymap(
 		"n",
 		M.config.keymap,
-		':lua require("diagnostic_comments").toggle_diagnostic_comments()<CR>',
+		':lua require("diagnostic-comments").toggle_diagnostic_comments()<CR>',
 		{ noremap = true, silent = true }
 	)
 	print("Diagnostic comments plugin setup complete")
@@ -22,25 +21,21 @@ end
 
 function M.update_diagnostic_comments()
 	print("Updating diagnostic comments")
-	-- Clear existing diagnostic comments
 	vim.api.nvim_buf_clear_namespace(0, M.namespace, 0, -1)
-	-- Get diagnostics for the current buffer
 	local diagnostics = vim.diagnostic.get(0)
 	print("Number of diagnostics: " .. #diagnostics)
 	for _, diagnostic in ipairs(diagnostics) do
 		local line = diagnostic.lnum
 		local message = diagnostic.message
 		local severity = diagnostic.severity
-		-- Create the comment text
-		local comment_text = string.format("-- %s: %s", vim.diagnostic.severity[severity], message)
+		local comment_text =
+			string.format("%s %s: %s", M.config.comment_prefix, vim.diagnostic.severity[severity], message)
 		if M.config.comment_style == "above" then
-			-- Add the comment on a new line above
 			vim.api.nvim_buf_set_extmark(0, M.namespace, line, 0, {
-				virt_lines = { { { "-- " .. vim.diagnostic.severity[severity] .. ": " .. message, "Comment" } } },
+				virt_lines = { { { comment_text, "Comment" } } },
 				virt_lines_above = true,
 			})
 		else
-			-- Add the comment to the end of the line as virtual text
 			vim.api.nvim_buf_set_extmark(0, M.namespace, line, 0, {
 				virt_text = { { comment_text, "Comment" } },
 				virt_text_pos = "eol",
